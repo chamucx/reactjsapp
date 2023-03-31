f79ffecb00ca1b1f28ef5a06248c53ec0a38a758
Antonio <joseantoniosysnet@gmail.com>
2023-03-31 05:37:42 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sigueme en los repos <code>mi Github</code> es el siguiente
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/chamucx/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
         </a>
       </header>
     </div>
